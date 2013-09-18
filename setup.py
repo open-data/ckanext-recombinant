f1ea0afe91f1f00acb84dbefb35394475a432a79
Ian Ward <ian@excess.org>
2013-09-18 09:49:59 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0.1'
+
+setup(
+    name='ckanext-recombinant',
+    version=version,
+    description="Create datastore tables for organizations and "
+        "provide combined output",
+    long_description="""
+    """,
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Government of Canada',
+    author_email='Michel.Gendron@statcan.gc.ca',
+    url='',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.recombinant'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points=\
+    """
+    [ckan.plugins]
+    recombinant=ckanext.recombinant.plugins:RecombinantPlugin
+
+    [paste.paster_command]
+    recombinant=ckanext.recombinant.commands:RecombinantCommand
+    """,
+)
