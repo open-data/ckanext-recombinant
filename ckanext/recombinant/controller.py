0ea619abcf120060974c6b64d7ef5f7d5e26f748
sklump <srklump@hotmail.com>
2015-03-10 10:33:59 -0400

    i18n for error messages where possible, let template handle unicode

--- ckanext/recombinant/controller.py
+++ ckanext/recombinant/controller.py
@@ -6,9 +6,10 @@ from ckan.controllers.package import PackageController
 from ckanext.recombinant.read_xls import read_xls, get_records
 from ckanext.recombinant.write_xls import xls_template
 from ckanext.recombinant.commands import _get_tables
-from ckan.logic import ValidationError
+from ckan.logic import ValidationError, NotAuthorized
 
 from cStringIO import StringIO
+from xlrd import XLRDError
 
 import ckanapi
 
@@ -18,63 +19,71 @@ class UploadController(PackageController):
     def upload(self, id):
         package_type = self._get_package_type(id)
         try:
-            lc = ckanapi.LocalCKAN(username = c.user)
-            package = lc.action.package_show(id = id)
+            lc = ckanapi.LocalCKAN(username=c.user)
+            package = lc.action.package_show(id=id)
             owner_org = package['organization']['name']
 
-            if not (h.check_access('package_update', {'id': id})):
-                raise ValidationError({'xls_update':
-                    _('You do not have permission to upload to {0}').format(
-                        owner_org)})
-
             if request.POST['xls_update'] == u'':
-                raise ValidationError(
-                    {'xls_update': _('You must provide a valid file')})
+                msg = 'You must provide a valid file'
+                raise ValidationError({'xls_update': {msg: ()}})
 
             upload_data = read_xls(
                 '',
                 file_contents = request.POST['xls_update'].file.read())
-            sheet_name, org_name = next(upload_data)
-
+            sheet_name, org_name = None, None
+            try:
+                sheet_name, org_name = next(upload_data)
+            except XLRDError, xerr:
+                msg = xerr.message
+                raise ValidationError({'xls_update': {msg: ()}})
 
             #is this the right sheet for this organization?
             if org_name != owner_org:
-                msg = _('Invalid sheet for this organization. ' +
+                msg = ('Invalid sheet for this organization. ' +
                     'Sheet must be labeled for {0}, ' +
-                    'but you supplied a sheet for {1}').format(
-                        owner_org,
-                        org_name)
-                raise ValidationError({'xls_update': msg})
+                    'but you supplied a sheet for {1}')
+                raise ValidationError(
+                    {'xls_update': {msg: (owner_org, org_name)}})
 
             for t in _get_tables():
                 if t['xls_sheet_name'] == sheet_name:
                     break
             else:
-                msg = _("Sheet name '{0}' not found in list of valid tables"
-                    ).format(sheet_name)
-                raise ValidationError({'xls_update': msg})
+                msg = "Sheet name '{0}' not found in list of valid tables"
+                raise ValidationError({'xls_update': {msg: (sheet_name,)}})
 
             resource_id = package['resources'][0]['id']
 
             records = get_records(upload_data, t['fields'])
-            lc.action.datastore_upsert(resource_id=resource_id, records=records)
+            try:
+                lc.action.datastore_upsert(
+                    resource_id=resource_id,
+                    records=records)
+            except NotAuthorized, na:
+                msg = 'You do not have permission to upload to {0}'
+                raise ValidationError({'xls_update': {msg: (owner_org,)}})
 
             h.flash_success(_(
                 "Your file was successfully uploaded into the central system."
                 ))
 
-            redirect(h.url_for(controller='package',
-                               action='read', id=id))
+            redirect(h.url_for(controller='package', action='read', id=id))
         except ValidationError, e:
             errors = []
+            # for error in e.error_dict.values():
+                # errors.append(str(error).decode('utf-8'))
             for error in e.error_dict.values():
-                errors.append(str(error).decode('utf-8'))
+                if isinstance(error, dict):
+                    errors.append(error)
+                else:
+                    errors.append(
+                        {str(error).decode('utf-8').replace('\n', '<br>'): ()})
             vars = {'errors': errors, 'action': 'edit'}
             c.pkg_dict = package
-            return render(self._edit_template(package_type), extra_vars = vars)
+            return render(self._edit_template(package_type), extra_vars=vars)
 
     def template(self, id):
-        lc = ckanapi.LocalCKAN(username = c.user)
+        lc = ckanapi.LocalCKAN(username=c.user)
         dataset = lc.action.package_show(id=id)
         org = lc.action.organization_show(id=dataset['owner_org'],
             include_datasets=False)
