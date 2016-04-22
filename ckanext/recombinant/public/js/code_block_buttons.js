d800cb0a4995f894278198f62ce33b78f9a6b111
Laurent Goderre <laurent.goderre@gmail.com>
2016-04-22 15:04:11 -0400

    Optimize the example syntaxes buttons

--- ckanext/recombinant/public/js/code_block_buttons.js
+++ ckanext/recombinant/public/js/code_block_buttons.js
@@ -1,14 +1,17 @@
-function langBtnFn() {
-  var codeBlockClass = "code-block", 
-    codeBtnSelector = ".btn-code-lang",
-    $codeBlock = $("." + codeBlockClass),
-    $codeBtn = $(codeBtnSelector),
-    lang = this.className.match(/lang\-[^\s]*/)[0];
-  if (lang) {
-    $codeBlock
-      .removeClass()
-      .addClass(codeBlockClass + " " + lang);
-    $codeBtn.removeClass("active");
-    $(this).addClass("active");
-  }
-};
+(function($){
+    var codeBtnSelector = ".btn-code-lang";
+
+    wb.doc.on("click", codeBtnSelector, function() {
+      var codeBlockClass = "code-block",
+        $codeBlock = $("." + codeBlockClass),
+        $codeBtn = $(codeBtnSelector),
+        lang = this.className.match(/lang\-[^\s]*/)[0];
+      if (lang) {
+        $codeBlock
+          .removeClass()
+          .addClass(codeBlockClass + " " + lang);
+        $codeBtn.removeClass("active");
+        $(this).addClass("active");
+      }
+    });
+})(jQuery);
