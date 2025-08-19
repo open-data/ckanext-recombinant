9e10d8f1b127526a63d7773ebae10baa44f87a90
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2025-08-19 19:20:58 +0000

    fix(js): wet tab issue;
    - Fixed issue with wb-tabs destroying event listeners.

--- ckanext/recombinant/assets/js/recombinant_callbacks.js
+++ ckanext/recombinant/assets/js/recombinant_callbacks.js
@@ -1,31 +1,36 @@
 window.addEventListener('load', function(){
   $(document).ready(function(){
 
-    let uploadField = $('.recombinant-upload-field');
-    if( uploadField.length > 0 ){
-      $(uploadField).each(function(_index, _uploadField){
-        // FIXME: not binding to node...
-        $(_uploadField).on('invalid', function(_event){
-          _uploadField[0].setCustomValidity($(_uploadField).attr('data-invalid-text'));
+    function _bind_recombinant_validity(){
+      let uploadField = $('.recombinant-upload-field');
+      if( uploadField.length > 0 ){
+        $(uploadField).each(function(_index, _uploadField){
+          $(_uploadField).off('invalid.CustomMessage');
+          $(_uploadField).on('invalid.CustomMessage', function(_event){
+            $(_uploadField)[0].setCustomValidity($(_uploadField).attr('data-invalid-text'));
+          });
+          $(_uploadField).on('change', function(_event){
+            $(_uploadField)[0].setCustomValidity('');
+          });
         });
-        $(_uploadField).on('change', function(_event){
-          console.log($(_uploadField).attr('data-invalid-text'));
-          _uploadField[0].setCustomValidity('');
+      }
+
+      let deleteField = $('.recombinant-delete-field');
+      if( deleteField.length > 0 ){
+        $(deleteField).each(function(_index, _deleteField){
+          $(_deleteField).off('invalid.CustomMessage');
+          $(_deleteField).on('invalid.CustomMessage', function(_event){
+            $(_deleteField)[0].setCustomValidity($(_deleteField).attr('data-invalid-text'));
+          });
+          $(_deleteField).on('change', function(_event){
+            $(_deleteField)[0].setCustomValidity('');
+          });
         });
-      });
+      }
     }
 
-    let deleteField = $('.recombinant-delete-field');
-    if( deleteField.length > 0 ){
-      $(deleteField).each(function(_index, _deleteField){
-        $(_deleteField).on('invalid', function(_event){
-          _deleteField[0].setCustomValidity($(_deleteField).attr('data-invalid-text'));
-        });
-        $(_deleteField).on('change', function(_event){
-          _deleteField[0].setCustomValidity('');
-        });
-      });
-    }
+    // NOTE: wet-boew tabs repaints contents of tab bodies, have to wait a bit...
+    setTimeout(_bind_recombinant_validity, 750);
 
     let orgSelect = $('.recombinant-org-field');
     if( orgSelect.length > 0 ){
