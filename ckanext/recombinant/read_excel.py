002a6dc8e5cb4daa6b820c30d15371c3bc0ee3f2
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2025-12-08 15:52:51 +0000

    Merge pull request #152 from open-data/fix/misc-exception-handling
    Improved Exception Handling on File Upload

--- ckanext/recombinant/read_excel.py
+++ ckanext/recombinant/read_excel.py
@@ -19,8 +19,8 @@ HEADER_ROWS_V3 = 5
 
 
 def read_excel(f: Union[str, FlaskFileStorage, FieldStorage],
-               expected_sheet_names: List[str] = [],
-               bad_sheet_names: List[str] = [],
+               expected_sheet_names: List[str] = [],  # type: ignore[reportCallInDefaultInitializer] # noqa: E501
+               bad_sheet_names: List[str] = [],  # type: ignore[reportCallInDefaultInitializer] # noqa: E501
                file_contents: Optional[str] = None) -> Iterator[Any]:
     """
     Return a generator that opens the excel file f (name or file object)
